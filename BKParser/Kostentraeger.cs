3b27c27438d600ab12ce0344ad099003059f95cd
gopnikada <klarsky.kirill6@gmail.com>
2023-08-08 00:00:45 +0200

    class created

---
+++ BKParser/Kostentraeger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKParser
+{
+    public class Kostentraeger
+    {
+        public int IK_Nr { get; set; } // IK-Nummer
+        public string Name { get; set; }
+        public string Adresse_StrasseHausnummer { get; set; } // Adresse Straße/Hausnummer
+        public int Adresse_PLZ { get; set; } // Adresse PLZ
+        public string Adresse_Ort { get; set; } // Adresse Ort
+        public int Id_Bezirk { get; set; }
+        public int Id_Bundesland { get; set; }
+        public string Email { get; set; }
+        public int IK_Nr_UebergeordneteIK { get; set; } // IK-Nr Übergeordnete IK
+        public short IK_Nr_Datenannahmestelle { get; set; } // IK-Nr Datenannahmestelle
+
+        public Kostentraeger(
+        int ikNr,
+        string name,
+        string adresseStrasseHausnummer,
+        int adressePLZ,
+        string adresseOrt,
+        int idBezirk,
+        int idBundesland,
+        string email,
+        int ikNrUebergeordneteIK,
+        short ikNrDatenannahmestelle)
+        {
+            IK_Nr = ikNr;
+            Name = name;
+            Adresse_StrasseHausnummer = adresseStrasseHausnummer;
+            Adresse_PLZ = adressePLZ;
+            Adresse_Ort = adresseOrt;
+            Id_Bezirk = idBezirk;
+            Id_Bundesland = idBundesland;
+            Email = email;
+            IK_Nr_UebergeordneteIK = ikNrUebergeordneteIK;
+            IK_Nr_Datenannahmestelle = ikNrDatenannahmestelle;
+        }
+    }
+    
+}
--- BKParser/Program.cs
+++ BKParser/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace BKParser
+﻿using System.Linq.Expressions;
+
+namespace BKParser
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = "C:\\Users\\User\\source\\repos\\BKParser\\BKParser\\aok.ke0";
+
+
+            var lines = File.ReadLines(filePath);
+
+            var kostTraegerList = new List<Kostentraeger>();
+
+            foreach (string line in lines)
+            {
+                string prefix = line[0..3];
+                switch (prefix)
+                {
+                    case "IDK":
+                        // code block
+                        break;
+                    case "UNA":
+                        // code block
+                        break;
+                    default:
+                        // code block
+                        break;
+                }
+
+            }
         }
     }
 }
